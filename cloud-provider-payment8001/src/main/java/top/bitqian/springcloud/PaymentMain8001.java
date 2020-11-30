57713e6ef4a2497c9277670f1afadb2abe207264
sevenyoungairye <849033901@qq.com>
2020-11-30 08:42:15 +0800

    初始mvn父工程环境。

---
+++ cloud-provider-payment8001/src/main/java/top/bitqian/springcloud/PaymentMain8001.java
@@ -0,0 +1,18 @@
+package top.bitqian.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author echo lovely
+ * @date 2020/11/29 23:01
+ */
+
+@SpringBootApplication
+public class PaymentMain8001 {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentMain8001.class, args);
+    }
+
+}
