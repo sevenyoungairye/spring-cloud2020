6eaa5596ad56a9c9bc412b2caa1cd5e03091c1bb
sevenyoungairye <849033901@qq.com>
2020-11-30 21:08:44 +0800

    完成了payment consumer消费者搭建，暴露端口80. get, restful~

---
+++ cloud-consumer-order/src/main/java/top/bitqian/springcloud/OrderMain80.java
@@ -0,0 +1,18 @@
+package top.bitqian.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author echo lovely
+ * @date 2020/11/30 19:28
+ */
+
+@SpringBootApplication
+public class OrderMain80 {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrderMain80.class, args);
+    }
+
+}
