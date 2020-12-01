3a19cb0786f4b706c16353994ab1f2ccc8b04b8f
sevenyoungairye <849033901@qq.com>
2020-12-01 17:55:51 +0800

    eureka服务客户端的编写，8001服务提供者，80服务消费者。

--- cloud-consumer-order80/src/main/java/top/bitqian/springcloud/OrderMain80.java
+++ cloud-consumer-order80/src/main/java/top/bitqian/springcloud/OrderMain80.java
@@ -2,12 +2,14 @@ package top.bitqian.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * @author echo lovely
  * @date 2020/11/30 19:28
  */
 
+@EnableEurekaClient // eureka客户端 服务消费者80
 @SpringBootApplication
 public class OrderMain80 {
 
--- cloud-provider-payment8001/src/main/java/top/bitqian/springcloud/PaymentMain8001.java
+++ cloud-provider-payment8001/src/main/java/top/bitqian/springcloud/PaymentMain8001.java
@@ -2,12 +2,15 @@ package top.bitqian.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * @author echo lovely
  * @date 2020/11/29 23:01
  */
 
+// 先启动服务注册中心7001，然后启动8001服务提供者 通过服务注册中心访问
+@EnableEurekaClient
 @SpringBootApplication
 public class PaymentMain8001 {
 
