a482dbb8c4fd4e749405d5176ce1714924ad06a3
sevenyoungairye <849033901@qq.com>
2020-12-01 19:00:38 +0800

    更改服务注册中心7001端口, 新增服务注册中心7002, 两个一起构成eureka集群。两者相互注册, 相互守望。

---
+++ cloud-eureka-server7002/src/main/java/top/bitqian/springcloud/EurekaMain7002.java
@@ -0,0 +1,21 @@
+package top.bitqian.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @author echo lovely
+ * @date 2020/12/1 18:52
+ */
+
+// 表明自己是服务注册中心. 地址7002
+@EnableEurekaServer
+@SpringBootApplication
+public class EurekaMain7002 {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaMain7002.class, args);
+    }
+
+}
