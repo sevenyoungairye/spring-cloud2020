f5e9ae0be6430fd50f7890672f321d8ed7071bd6
sevenyoungairye <849033901@qq.com>
2020-12-06 17:37:53 +0800

    eureka整合ribbon实现客户端负载均衡。response entity调用。

--- cloud-consumer-order80/src/main/java/top/bitqian/springcloud/controller/OrderController.java
+++ cloud-consumer-order80/src/main/java/top/bitqian/springcloud/controller/OrderController.java
@@ -1,6 +1,7 @@
 package top.bitqian.springcloud.controller;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -37,7 +38,7 @@ public class OrderController {
 
     // 调用
     @GetMapping("/consumer/payment/create")
-    public CommonResult<Payment> create(Payment payment) {
+    public CommonResult create(Payment payment) {
 
         log.info("嘤嘤怪，跑通了吖。");
         // 调用8001服务接口..
@@ -46,9 +47,49 @@ public class OrderController {
 
     // 调用查询~
     @GetMapping("/consumer/payment/get/{id}")
-    public CommonResult<Payment> getPayment(@PathVariable("id") Long id) {
+    public CommonResult getPayment(@PathVariable("id") Long id) {
         log.info("嘤嘤怪，跑通了吖。消费者80调用8001..");
         return restTemplate.getForObject(PAYMENT_URL + "/payment/get/" + id, CommonResult.class);
+    }
+
+
+    // restTemplate~
+    // https://docs.spring.io/spring-framework/docs/
+    // 5.2.2.RELEASE/javadoc-api/org/springframework/web/client/RestTemplate.html
+
+    // getForEntity 返回状态码，响应头，响应体等..
+    @GetMapping("/consumer/payment/getByEntity/{id}")
+    public CommonResult getPaymentByEntity(@PathVariable("id") Long id) {
+
+        ResponseEntity<CommonResult> entity =
+                restTemplate.getForEntity(PAYMENT_URL + "/payment/get/" + id, CommonResult.class);
+
+        if (entity.getStatusCode().is2xxSuccessful()) {
+            log.info("调用成功~" + entity.getHeaders());
+
+            return entity.getBody();
+        }
+
+        return new CommonResult<>(444, "系统故障...调用失败~");
+    }
+
+    // postForObject/postForEntity
+    // postForEntity 新增
+    @GetMapping("/consumer/payment/createByPostEntity")
+    public CommonResult<Payment> createByPostEntity(Payment payment) {
+
+        // 返回responseBody对象
+        ResponseEntity<CommonResult> res = restTemplate.
+                postForEntity(PAYMENT_URL + "/payment/create", payment, CommonResult.class);
+
+        log.info("res------>" + res.getBody().toString());
+
+        if (res.getStatusCode().is2xxSuccessful()) {
+
+            return res.getBody();
+        }
+
+        return new CommonResult(444, "error");
     }
 
 }
