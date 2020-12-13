4d4d703ab0ff10997045191b503ec67bc03955ed
sevenyoungairye <849033901@qq.com>
2020-12-13 20:24:53 +0800

    解决了客户端配置同步, 需要重启微服务的问题。发送post请求可。但是机器多了, 脚本解决。但是我想某些机器需要更新配置, 而某些不需要。

--- cloud-config-client-3355/src/main/java/top/bitqian/springcloud/controller/ConfigClientController.java
+++ cloud-config-client-3355/src/main/java/top/bitqian/springcloud/controller/ConfigClientController.java
@@ -1,6 +1,7 @@
 package top.bitqian.springcloud.controller;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
  * @date 2020/12/13 17:44
  */
 
+@RefreshScope // 实现动态刷新,
 @RestController
 public class ConfigClientController {
 
