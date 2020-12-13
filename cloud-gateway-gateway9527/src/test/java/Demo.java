8fcac6fbc6adedaa13a97c85147a5f44c612bffc
sevenyoungairye <849033901@qq.com>
2020-12-13 12:09:33 +0800

    网关断言配置规则.

---
+++ cloud-gateway-gateway9527/src/test/java/Demo.java
@@ -0,0 +1,16 @@
+import java.time.ZonedDateTime;
+
+/**
+ * @author echo lovely
+ * @date 2020/12/13 11:33
+ */
+
+public class Demo {
+
+    public static void main(String[] args) {
+
+        ZonedDateTime now = ZonedDateTime.now();
+        System.out.println(now);
+    }
+
+}
