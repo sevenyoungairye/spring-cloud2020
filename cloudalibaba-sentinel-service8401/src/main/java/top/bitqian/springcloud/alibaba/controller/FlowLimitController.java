4d798ebf43464a60b65229c2cf9b20e7e2497ce7
sevenyoungairye <849033901@qq.com>
2020-12-17 12:10:16 +0800

    流量控制 测试了1s一个请求QPS, 1s 一个线程。 Blocked by Sentinel (flow limiting)

--- cloudalibaba-sentinel-service8401/src/main/java/top/bitqian/springcloud/alibaba/controller/FlowLimitController.java
+++ cloudalibaba-sentinel-service8401/src/main/java/top/bitqian/springcloud/alibaba/controller/FlowLimitController.java
@@ -4,6 +4,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Date;
+import java.util.concurrent.TimeUnit;
 
 /**
  * @author echo lovely
@@ -15,6 +16,13 @@ public class FlowLimitController {
 
     @GetMapping("/test/a")
     public String getA() {
+
+        // 流控: 睡0.8s 测试线程数 1s 一个线程
+        try {
+            TimeUnit.MILLISECONDS.sleep(800);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
 
         return "A \t" + new Date(System.currentTimeMillis());
     }
