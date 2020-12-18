a99a872a5fe9a2ad5345e8cdd1b1ac3ff0363286
sevenyoungairye <849033901@qq.com>
2020-12-18 22:47:35 +0800

    指定服务出现的异常不做处理。

--- cloudalibaba-consumer-nacos-order84/src/main/java/top/bitqian/springcloud/alibaba/controller/CircleBreakerController.java
+++ cloudalibaba-consumer-nacos-order84/src/main/java/top/bitqian/springcloud/alibaba/controller/CircleBreakerController.java
@@ -33,7 +33,10 @@ public class CircleBreakerController
     @GetMapping("/consumer/fallback/{id}")
     // @SentinelResource(value = "consumer/fallback", fallback = "fallbackHandler") // fallback 出现业务错误了该怎么办
     // @SentinelResource(value = "consumer/fallback", blockHandler = "blockExceptionHandler") // block 不符合sentinel控制台规则
-    @SentinelResource(value = "consumer/fallback", blockHandler = "blockExceptionHandler", fallback = "fallbackHandler") // block & java exception
+    // @SentinelResource(value = "consumer/fallback", blockHandler = "blockExceptionHandler", fallback = "fallbackHandler") // block & java exception
+    @SentinelResource(value = "consumer/fallback", blockHandler = "blockExceptionHandler",
+            fallback = "fallbackHandler",
+            exceptionsToIgnore = {NullPointerException.class}) // ignore exp will throw the ex
     public CommonResult<Payment> fallback(@PathVariable("id") Long id)
     {
 
